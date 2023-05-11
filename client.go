1040a4b8dc0bd34af900d720071240ec9207cb0c
neymarjimoh <jemohkunle2007@gmail.com>
2023-05-11 15:01:23 +0100

    add http request handler

--- client.go
+++ client.go
@@ -1,6 +1,10 @@
 package fincra
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"time"
@@ -48,4 +52,42 @@ func getBaseUrl(sandbox bool) *url.URL {
 		return url
 	}
 	return url
+}
+
+type Response map[string]interface{}
+
+func (c *Client) sendRequest(method, path string, payload interface{}) ([]byte, error) {
+	var buf io.ReadWriter
+	if payload != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(payload)
+		if err != nil {
+			return nil, fmt.Errorf("%w", err)
+		}
+	}
+
+	url := fmt.Sprintf("%s%s", c.BaseUrl.String(), path)
+
+	req, err := http.NewRequest(method, url, buf)
+	if err != nil {
+		return nil, fmt.Errorf("error instantiating request: %w", err)
+	}
+
+	req.Header.Set("Accept", "application/json; charset=utf-8")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("api-key", c.apiKey)
+
+	response, err := c.HttpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+
+	defer response.Body.Close()
+
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read response from body: %w", err)
+	}
+	
+	return responseBody, nil
 }
