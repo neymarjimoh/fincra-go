4e37947f403b8d7efad24549764cf257c097f771
neymarjimoh <jemohkunle2007@gmail.com>
2023-10-08 13:38:21 +0100

    add virtual account module

--- vaccounts.go
+++ vaccounts.go
@@ -1,6 +1,12 @@
 package fincra
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/neymarjimoh/fincra-go/utils"
+)
 
 const (
 	virtualAccountUrl = "/profile/virtual-accounts/"
@@ -43,12 +49,104 @@ type DocumentDto struct {
 	ExpirationDate    string `json:"expirationDate,omitempty"`
 }
 
+type Options struct {
+	Currency      string `json:"currency"`
+	BusinessName  string `json:"businessName"`
+	IssuedDate    string `json:"issuedDate"`
+	RequestedDate string `json:"requestedDate"`
+	AccountNumber string `json:"accountNumber"`
+	Status        string `json:"status"`
+}
+
 // read here https://docs.fincra.com/reference/request-virtual-accounts before using this method
 func (c *Client) CreateVirtualAccount(data CreateVirtualAccountDto) (Response, error) {
 
 	path := virtualAccountUrl + "/requests"
 
 	response, err := c.sendRequest("POST", path, data)
+
+	_ = json.Unmarshal(response, &jsonResponse)
+
+	return jsonResponse, err
+}
+
+// read here https://docs.fincra.com/reference/get-merchant-virtual-account-requests for more info on this method
+func (c *Client) ListVirtualAccounts(options Options) (Response, error) {
+	if utils.IsEmpty(options) {
+		return nil, fmt.Errorf("at least one option (currency, businessName, issuedDate, requestedDate, accountNumber, status) must be specified")
+	}
+
+	var queryParameters []string
+
+	if options.Currency != "" {
+		queryParameters = append(queryParameters, "currency="+options.Currency)
+	}
+	if options.BusinessName != "" {
+		queryParameters = append(queryParameters, "businessName="+options.BusinessName)
+	}
+	if options.IssuedDate != "" {
+		queryParameters = append(queryParameters, "issuedDate="+options.IssuedDate)
+	}
+	if options.RequestedDate != "" {
+		queryParameters = append(queryParameters, "requestedDate="+options.RequestedDate)
+	}
+	if options.AccountNumber != "" {
+		queryParameters = append(queryParameters, "accountNumber="+options.AccountNumber)
+	}
+	if options.Status != "" {
+		queryParameters = append(queryParameters, "status="+options.Status)
+	}
+
+	path := virtualAccountUrl
+	if len(queryParameters) > 0 {
+		path += "?" + strings.Join(queryParameters, "&")
+	}
+
+	response, err := c.sendRequest("GET", path, nil)
+
+	_ = json.Unmarshal(response, &jsonResponse)
+
+	return jsonResponse, err
+}
+
+// This method is used for getting all account requests belonging to a merchant
+func (c *Client) ListVirtualAccountRequests() (Response, error) {
+	path := virtualAccountUrl + "requests"
+
+	response, err := c.sendRequest("GET", path, nil)
+
+	_ = json.Unmarshal(response, &jsonResponse)
+
+	return jsonResponse, err
+}
+
+// This method is used for retrieving an account that is belongs to a merchant by currency
+func (c *Client) ListVirtualAccountByCurrency(currency string) (Response, error) {
+	path := virtualAccountUrl + "?currency=" + currency
+
+	response, err := c.sendRequest("GET", path, nil)
+
+	_ = json.Unmarshal(response, &jsonResponse)
+
+	return jsonResponse, err
+}
+
+// This method is used for retrieving an account that is belongs to a merchant by BVN
+func (c *Client) ListVirtualAccountByBvn(bvn, businessId string) (Response, error) {
+	path := virtualAccountUrl + "/bvn?bvn=" + bvn + "&business=" + businessId
+
+	response, err := c.sendRequest("GET", path, nil)
+
+	_ = json.Unmarshal(response, &jsonResponse)
+
+	return jsonResponse, err
+}
+
+// This method is used for retrieving a virtual account
+func (c *Client) ListVirtualAccount(accountId string) (Response, error) {
+	path := virtualAccountUrl + "/virtual-accounts/" + accountId
+
+	response, err := c.sendRequest("GET", path, nil)
 
 	_ = json.Unmarshal(response, &jsonResponse)
 
--- vaccounts_test.go
+++ vaccounts_test.go
@@ -10,12 +10,13 @@ func TestCreateVirtualAccount(t *testing.T) {
 		Currency:    "NGN",
 		UtilityBill: "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
 		AccountType: "individual",
-		KYCInformation: KYCInformation{
+		KYCInformation: KYCInformationDto{
 			FirstName:    "John",
 			LastName:     "Doe",
 			Email:        "abc@abc.com",
 			BusinessName: "JohnDoe",
 		},
+		Channel: "providus",
 	}
 
 	t.Run("create virtual account", func(t *testing.T) {
@@ -24,6 +25,117 @@ func TestCreateVirtualAccount(t *testing.T) {
 		resp, err := client.CreateVirtualAccount(request)
 		if err != nil {
 			t.Errorf("error creating virtual account: %v", err)
+		}
+
+		fmt.Println(resp)
+	})
+}
+
+func TestListVirtualAccounts(t *testing.T) {
+	t.Run("list virtual accounts", func(t *testing.T) {
+		client := defaultTestClient()
+
+		options := Options{
+			Currency: "EUR",
+		}
+
+		resp, err := client.ListVirtualAccounts(options)
+		if err != nil {
+			t.Errorf("error listing virtual accounts: %v", err)
+		}
+
+		fmt.Println(resp)
+
+		want := map[string]interface{}{
+			"success": true,
+			"message": "Merchant virtual accounts fetched successfully",
+		}
+
+		got := make(map[string]interface{}, len(want))
+		for k, v := range resp {
+			if k == "message" || k == "success" {
+				got[k] = v
+			}
+		}
+
+		testEqual(t, got, want)
+	})
+}
+
+func TestListVirtualAccountRequests(t *testing.T) {
+	t.Run("list virtual account requests", func(t *testing.T) {
+		client := defaultTestClient()
+
+		resp, err := client.ListVirtualAccountRequests()
+		if err != nil {
+			t.Errorf("error listing virtual account requests: %v", err)
+		}
+
+		fmt.Println(resp)
+
+		want := map[string]interface{}{
+			"success": true,
+			"message": "Virtual account requests fetched successfully",
+		}
+
+		got := make(map[string]interface{}, len(want))
+		for k, v := range resp {
+			if k == "message" || k == "success" {
+				got[k] = v
+			}
+		}
+
+		testEqual(t, got, want)
+	})
+}
+
+func TestListVirtualAccountByCurrency(t *testing.T) {
+	t.Run("Fetch a virtual account by currency", func(t *testing.T) {
+		client := defaultTestClient()
+
+		resp, err := client.ListVirtualAccountByCurrency("EUR")
+		if err != nil {
+			t.Errorf("error fetching a virtual account by currency: %v", err)
+		}
+
+		fmt.Println(resp)
+
+		want := map[string]interface{}{
+			"success": true,
+			"message": "Merchant virtual accounts fetched successfully",
+		}
+
+		got := make(map[string]interface{}, len(want))
+		for k, v := range resp {
+			if k == "message" || k == "success" {
+				got[k] = v
+			}
+		}
+
+		testEqual(t, got, want)
+	})
+}
+
+func TestListVirtualAccountByBvn(t *testing.T) {
+	t.Run("Fetch a virtual account by BVN", func(t *testing.T) {
+		client := defaultTestClient()
+
+		resp, err := client.ListVirtualAccountByBvn("0123456789", "6457d39b12b4401f99a54772")
+		if err != nil {
+			t.Errorf("error fetching a virtual account by BVN: %v", err)
+		}
+
+		fmt.Println(resp)
+	})
+}
+
+func TestListVirtualAccount(t *testing.T) {
+	t.Run("Fetch a virtual account", func(t *testing.T) {
+		client := defaultTestClient()
+
+		resp, err := client.ListVirtualAccount("6457d39b12b4401f99a54772")
+		if err != nil {
+			t.Errorf("error fetching a virtual account: %v", err)
 		}
 
 		fmt.Println(resp)
